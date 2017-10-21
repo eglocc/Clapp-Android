6c0635e190e99f90b30fc3cd7619102722c4ae47
Ergiz Gizer <ergizgizer@gmail.com>
2017-10-21 20:30:29 +0300

    Add gradle-wrapper.jar

--- app/src/main/java/clappapp/club/clapp/AuthActivity.java
+++ app/src/main/java/clappapp/club/clapp/AuthActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
